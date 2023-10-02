bf07c03a301dd3eba5d10777d1ad799527a0ed72
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2023-10-02 12:54:14 -0700

    m

--- src/main/java/com/ple/visur/CanvasWasChangedVerticle.java
+++ src/main/java/com/ple/visur/CanvasWasChangedVerticle.java
@@ -2,19 +2,22 @@ package com.ple.visur;
 
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava3.core.eventbus.Message;
 
 public class CanvasWasChangedVerticle extends AbstractVisurVerticle {
   @Override
   public void start() {
-    vertx.eventBus().consumer(BusEvent.canvasWasChanged.name(), event -> {
-      JsonObject canvasJson = new JsonObject((String)event.body());
-      final Integer width = canvasJson.getInteger("width");
-      modelInt.put("canvasWidth", width);
-      final Integer height = canvasJson.getInteger("height");
-      modelInt.put("canvasHeight", height);
-      System.out.println("canvas width = " + width);
-      System.out.println("canvas height = " + height);
-    });
+    vertx.eventBus().consumer(BusEvent.canvasWasChanged.name(), this::handle);
+  }
+
+  public void handle(Message event) {
+    JsonObject canvasJson = new JsonObject((String)event.body());
+    final Integer width = canvasJson.getInteger("width");
+    modelInt.put("canvasWidth", width);
+    final Integer height = canvasJson.getInteger("height");
+    modelInt.put("canvasHeight", height);
+    System.out.println("canvas width = " + width);
+    System.out.println("canvas height = " + height);
   }
 
 }
