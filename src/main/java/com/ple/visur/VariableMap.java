be6b0fc54d47fa4f5a0924ccab3a6a5dcfaf420a
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-01-29 10:51:29 -0700

    store contentX and contentY in globalVariableMap as IntVisurVars nr

--- src/main/java/com/ple/visur/VariableMap.java
+++ src/main/java/com/ple/visur/VariableMap.java
@@ -3,21 +3,21 @@ package com.ple.visur;
 import java.util.HashMap;
 
 public class VariableMap {
-  HashMap<String, StringVisurVar> val;
+  HashMap<String, VisurVar> val;
 
-  public VariableMap(HashMap<String, StringVisurVar> map) {
+  public VariableMap(HashMap<String, VisurVar> map) {
     val = map;
   }
 
-  public static VariableMap make(HashMap<String, StringVisurVar> map) {
+  public static VariableMap make(HashMap<String, VisurVar> map) {
     return new VariableMap(map);
   }
 
-  public void put(String key, StringVisurVar var) {
+  public void put(String key, VisurVar var) {
     val.put(key, var);
   }
 
-  public StringVisurVar get(String key) {
+  public VisurVar get(String key) {
     return val.get(key);
   }
 
