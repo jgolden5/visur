15c63f8bb5c32ba173112a9050e292306fe57e54
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-02-05 12:52:51 -0700

    set up the compile method structure for word checking

--- src/main/java/com/ple/visur/StringToCommandService.java
+++ src/main/java/com/ple/visur/StringToCommandService.java
@@ -1,7 +1,37 @@
 package com.ple.visur;
 
+import java.util.ArrayList;
+
 public class StringToCommandService {
   public VisurCommand compile(String sentence) {
-    return null;
+    boolean sentenceCanStillBeCompiled = true;
+    VisurCommand command = new VisurCommand();
+    ArrayList<Word> words = new ArrayList<>();
+    while(sentenceCanStillBeCompiled) {
+      int i = 0;
+      while (i < 5) {
+        String regex;
+        switch (i) {
+          case 0:
+            regex = ""; //LiteralNumberWord
+            break;
+          case 1:
+            regex = ""; //LiteralStringWord
+            break;
+          case 2:
+            regex = ""; //AssignmentWord
+            break;
+          case 3:
+            regex = ""; //OperatorWord
+            break;
+          case 4:
+            regex = ""; //RecallWord
+            break;
+          default:
+            System.out.println("iterator id not recognized");
+        }
+      }
+    }
+    return command;
   }
 }
