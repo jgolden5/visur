e8ebe2f64ffecac5935ccc33ec3ebfbacfa8414a
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-02-08 08:47:13 -0700

    get rid of all the old stuff in CommandCompileService besides switch statement & regexes nr

--- src/main/java/com/ple/visur/CommandCompileService.java
+++ src/main/java/com/ple/visur/CommandCompileService.java
@@ -21,28 +21,23 @@ public class CommandCompileService {
       switch (i) {
         case 0:
           pattern = Pattern.compile("^(-?[0-9]+\\.?[0-9]*)(.*)");
-          wordToAddToCommand = LiteralNumberWord.make();
-          newSentence = modifyCommandAndGetRemainingSentence(currentSentence, pattern, command, wordToAddToCommand);
+
           break;
         case 1:
           pattern = Pattern.compile("(\"[^\"]*\")(.*)");
-          wordToAddToCommand = LiteralStringWord.make();
-          newSentence = modifyCommandAndGetRemainingSentence(currentSentence, pattern, command, wordToAddToCommand);
+
           break;
         case 2:
           pattern = Pattern.compile("(->*[^\s]+)(.*)");
-          wordToAddToCommand = AssignmentWord.make();
-          newSentence = modifyCommandAndGetRemainingSentence(currentSentence, pattern, command, wordToAddToCommand);
+
           break;
         case 3:
           pattern = Pattern.compile("([^\\s]+)(.*)");
-          wordToAddToCommand = NativeOperatorWord.make();
-          newSentence = modifyCommandAndGetRemainingSentence(currentSentence, pattern, command, wordToAddToCommand);
+
           break;
         case 4:
           pattern = Pattern.compile("([^\\s]+)(.*)");
-          wordToAddToCommand = RecallWord.make();
-          newSentence = modifyCommandAndGetRemainingSentence(currentSentence, pattern, command, wordToAddToCommand);
+
           break;
         default:
           System.out.println("word not recognized!");
@@ -57,62 +52,6 @@ public class CommandCompileService {
       }
     }
     return command;
-  }
-
-  private boolean operatorExistsForWord(String word) {
-    for (Operator op : Operator.values()) {
-      if (op.name().equals(word)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private boolean globalVarExistsForWord(String word) {
-    VariableMap gvm = ServiceHolder.editorModelService.getGlobalVariableMap();
-    return gvm.get(word) != null;
-  }
-
-  private String modifyCommandAndGetRemainingSentence(String currentSentence, Pattern pattern, VisurCommand command, Word wordToAddToCommand) {
-    Matcher matcher = pattern.matcher(currentSentence);
-    if (matcher.matches()) {
-      Object wordValue = null;
-      Operator operatorFromWord;
-      operatorFromWord = wordToAddToCommand.toOperator();
-      switch(operatorFromWord) {
-        case LiteralNumberOperator n -> {
-          wordValue = Integer.parseInt(matcher.group(1));
-        }
-        case LiteralStringOperator s -> {
-          wordValue = matcher.group(1);
-        }
-        case AssignmentOperator a -> {
-          wordValue = EditorModelKey.valueOf(matcher.group(1).substring(2)); //chop off the ->
-        }
-//        case NativeOperator n ->
-//          String currentWord = matcher.group(1);
-//          if(currentWord.equals("+")) {
-//            currentWord = "add";
-//          }
-//          if(operatorExistsForWord(currentWord)) {
-//            wordValue = Operator.valueOf(currentWord);
-//          }
-//          break;
-        case RecallOperator r -> {
-          if (globalVarExistsForWord(matcher.group(1))) {
-            wordValue = matcher.group(1);
-          }
-        }
-        default -> System.out.println("Word Operator " + wordToAddToCommand.toOperator() + " not recognized");
-      }
-      if(wordValue != null) {
-        currentSentence = matcher.group(2);
-        Object[] opAndOpInfo = new Object[]{operatorFromWord, wordValue};
-        command.addOperatorWithInfo(opAndOpInfo);
-        System.out.println("matched word = \"" + wordValue + "\", remaining = \"" + currentSentence + "\".");
-      }
-    }
-    return currentSentence;
   }
 
 }
