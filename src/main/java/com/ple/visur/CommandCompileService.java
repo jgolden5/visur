d365ed362f578899dd9e583fe11689b595b3e8e2
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-02-08 12:34:00 -0700

    simplify compile loop nr

--- src/main/java/com/ple/visur/CommandCompileService.java
+++ src/main/java/com/ple/visur/CommandCompileService.java
@@ -12,52 +12,23 @@ public class CommandCompileService {
     VisurCommand command = new VisurCommand(sentence);
     String currentSentence = sentence;
 
-    int i = 0;
-    compileLoop:
     while(currentSentence != "") {
-      CompiledWordResponse compiledWord;
-      switch (i) {
-        case 0:
-          LiteralNumberWord literalNumberWord = new LiteralNumberWord();
-          compiledWord = literalNumberWord.compile(currentSentence);
-          break;
-        case 1:
-          LiteralStringWord literalStringWord = new LiteralStringWord();
-          compiledWord = literalStringWord.compile(currentSentence);
-          break;
-        case 2:
-          pattern = Pattern.compile("(->*[^\s]+)(.*)");
-          matcher = getMatcher(pattern, currentSentence);
-          opInfo = EditorModelKey.valueOf(matcher.group(1));
-          op = AssignmentWord.make().toOperator();
-          break;
-        case 3:
-          pattern = Pattern.compile("([^\\s]+)(.*)");
-          matcher = getMatcher(pattern, currentSentence);
-          opInfo = matcher.group(1);
-          op = NativeOperatorWord.make((String)opInfo).toOperator();
-          break;
-        case 4:
-          pattern = Pattern.compile("([^\\s]+)(.*)");
-          matcher = getMatcher(pattern, currentSentence);
-          if(varExistsInEditorModel(matcher.group(1))) {
-            opInfo = matcher.group(1);
-          }
-          op = RecallWord.make().toOperator();
-          break;
-        default:
-          System.out.println("word not recognized!");
-          command = null;
-          break compileLoop;
+      CompiledWordResponse compiledWordResponse;
+
+      LiteralNumberWord literalNumberWord = new LiteralNumberWord();
+      LiteralStringWord literalStringWord = new LiteralStringWord();
+      AssignmentWord assignmentWord = new AssignmentWord();
+      NativeOperatorWord nativeOperatorWord = new NativeOperatorWord();
+      RecallWord recallWord = new RecallWord();
+      Word[] words = new Word[] {
+        literalNumberWord, literalStringWord, assignmentWord, nativeOperatorWord, recallWord
+      };
+
+      for(Word word : words) {
+        compiledWordResponse = word.compile(currentSentence);
+        currentSentence = compiledWordResponse.remainingSentence;
       }
-      if(matcher == null) {
-        i++;
-      } else {
-        i = 0;
-        currentSentence = matcher.group(2).stripLeading();
-        command.ops.add(op);
-        command.opInfo.add(opInfo);
-      }
+
     }
     return command;
   }
