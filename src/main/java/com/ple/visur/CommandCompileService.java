8d36ea0db4e6ed4f3e685b79829784171804f58b
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-02-08 08:50:07 -0700

    made wordInstance.toOperator() in CommandCompileService nr

--- src/main/java/com/ple/visur/CommandCompileService.java
+++ src/main/java/com/ple/visur/CommandCompileService.java
@@ -18,33 +18,34 @@ public class CommandCompileService {
       Pattern pattern;
       Word wordToAddToCommand;
       String newSentence = "";
+      Operator op;
       switch (i) {
         case 0:
           pattern = Pattern.compile("^(-?[0-9]+\\.?[0-9]*)(.*)");
-
+          op = LiteralNumberWord.make().toOperator();
           break;
         case 1:
           pattern = Pattern.compile("(\"[^\"]*\")(.*)");
-
+          op = LiteralStringWord.make().toOperator();
           break;
         case 2:
           pattern = Pattern.compile("(->*[^\s]+)(.*)");
-
+          op = AssignmentWord.make().toOperator();
           break;
         case 3:
           pattern = Pattern.compile("([^\\s]+)(.*)");
-
+          op = NativeOperatorWord.make().toOperator();
           break;
         case 4:
           pattern = Pattern.compile("([^\\s]+)(.*)");
-
+          op = RecallWord.make().toOperator();
           break;
         default:
           System.out.println("word not recognized!");
           command = null;
           break compileLoop;
       }
-      if(currentSentence.equals(newSentence)) {
+      if(noMatchFound) {
         i++;
       } else {
         i = 0;
