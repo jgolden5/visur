d02f1b4193d0044875e4983cec2639222623f282
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-02-09 11:23:25 -0700

    bug fix ExecutionStateStack does not get added to editorModel (implement Shareable) nr

--- src/main/java/com/ple/visur/ExecutionState.java
+++ src/main/java/com/ple/visur/ExecutionState.java
@@ -1,7 +1,9 @@
 package com.ple.visur;
 
+import io.vertx.core.shareddata.Shareable;
+
 import java.util.Stack;
 
-public class ExecutionState {
+public class ExecutionState implements Shareable {
   Stack<Object> stack = new Stack<>();
 }
