8ca0dd2280ad3611b3d854d443a35611b5f5d669
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-02-12 14:39:06 -0700

    basic implementation of absoluteMove (not working) rwb

--- src/main/java/com/ple/visur/AbsoluteMoveOperator.java
+++ src/main/java/com/ple/visur/AbsoluteMoveOperator.java
@@ -3,6 +3,11 @@ package com.ple.visur;
 public class AbsoluteMoveOperator implements Operator {
   @Override
   public void execute(Object opInfo) {
-    
+    EditorModelService ems = ServiceHolder.editorModelService;
+    ExecutionData eds = ems.getExecutionData();
+    IntVisurVar contentY = new IntVisurVar((int)eds.stack.pop());
+    IntVisurVar contentX = new IntVisurVar((int)eds.stack.pop());
+    ems.putGlobalVar("contentX", contentX);
+    ems.putGlobalVar("contentY", contentY);
   }
 }
