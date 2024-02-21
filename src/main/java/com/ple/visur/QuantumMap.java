5be0dbc8911da90418b0d70449f812d0b54f789b
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-02-21 11:08:21 -0700

    created QuantumMap class

---
+++ src/main/java/com/ple/visur/QuantumMap.java
@@ -0,0 +1,12 @@
+package com.ple.visur;
+
+import java.util.HashMap;
+
+public class QuantumMap {
+  HashMap<String, Quantum> map = new HashMap<>();
+
+  public QuantumMap(HashMap<String, Quantum> map) {
+    this.map = map;
+  }
+
+}
