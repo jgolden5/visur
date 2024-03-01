1b8c6f6e89c55e0554bd92a9fad1573d9fea7f04
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-03-01 10:18:22 -0700

    fix ChangeQuantumOp.execute nr

--- src/main/java/com/ple/visur/ChangeQuantumOp.java
+++ src/main/java/com/ple/visur/ChangeQuantumOp.java
@@ -1,16 +1,16 @@
 package com.ple.visur;
 
-public class ChangeQuantum implements Operator {
+public class ChangeQuantumOp implements Operator {
 
   @Override
   public void execute(Object opInfo) {
     EditorModelService ems = ServiceHolder.editorModelService;
     String quantumName = (String)ems.getExecutionDataStack().pop();
     Quantum targetQuantum = ems.getQuantumMap().get(quantumName);
-    String[] contentLines = ems.getEditorContent();
+    String editorContent = ems.getEditorContent();
     int contentX = ems.getGlobalVar("contentX").getInt();
     int contentY = ems.getGlobalVar("contentY").getInt();
-    int[] bounds = targetQuantum.getBoundaries(contentLines, contentX, contentY);
+    int[] bounds = targetQuantum.getBoundaries(editorContent, contentX, contentY);
     ems.putQuantumStart(bounds[0]);
     ems.putQuantumEnd(bounds[1]);
   }
