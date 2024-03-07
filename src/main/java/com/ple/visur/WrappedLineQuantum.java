a40e401aadf243123974df2ca4f919946631fac3
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-03-07 09:02:12 -0700

    implement WrappedLineQuantum.getBoundaries()

--- src/main/java/com/ple/visur/WrappedLineQuantum.java
+++ src/main/java/com/ple/visur/WrappedLineQuantum.java
@@ -1,14 +1,33 @@
 package com.ple.visur;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 
 public class WrappedLineQuantum implements Quantum {
 
   @Override
   public int[] getBoundaries(String editorContent, ArrayList<Integer> newlineIndices, int x, int y, boolean includeTail) {
-    int startBound = 0;
-    int endBound = 0;
-    return new int[]{startBound, endBound};
+    boolean lowerBoundFound = false; //check for lowerBound first
+    boolean upperBoundFound = false; //if lowerBoundFound, check for upperBound
+    int[] bounds = new int[]{x, x};
+    boolean lowerAndUpperBoundsFound = false;
+    while (!lowerAndUpperBoundsFound) {
+      if(!lowerBoundFound) {
+        if (x <= 0 || editorContent.charAt(x - 1) == '\n') {
+          lowerBoundFound = true;
+        } else {
+          bounds[x]--;
+        }
+      } else {
+        if(x >= editorContent.length() - 1 && editorContent.charAt(x + 1) != '\n') {
+          bounds[x]++;
+        } else {
+          upperBoundFound = true;
+        }
+      }
+      lowerAndUpperBoundsFound = lowerBoundFound && upperBoundFound;
+    }
+    return bounds;
   }
 
   @Override
