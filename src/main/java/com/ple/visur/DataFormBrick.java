4c48041c3c9e9789dd0e82ade2e548ccfea9884a
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-04-04 12:42:22 -0700

    ^ DataFormBrick nr

---
+++ src/main/java/com/ple/visur/DataFormBrick.java
@@ -0,0 +1,50 @@
+package com.ple.visur;
+
+import java.util.Objects;
+
+public class DataFormBrick {
+  DataForm fromDF;
+  Object val;
+
+
+  public DataFormBrick(DataForm fromDF, Object val) {
+    this.fromDF = fromDF;
+    this.val = val;
+  }
+
+  public static DataFormBrick make(DataForm fromDF, Object val) {
+    return new DataFormBrick(fromDF, val);
+  }
+
+  public DataFormBrick convert(DataForm toDF) {
+    Object newDFBVal = fromDF.convert(toDF, val);
+    return new DataFormBrick(toDF, newDFBVal);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    DataFormBrick that = (DataFormBrick) o;
+    boolean thisValEqualsThatVal;
+    if(val == null) {
+      thisValEqualsThatVal = val == that.val;
+    } else {
+      thisValEqualsThatVal = val.equals(that.val);
+    }
+    return fromDF.equals(that.fromDF) && thisValEqualsThatVal;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(fromDF, val);
+  }
+
+  public Object getVal() {
+    return val;
+  }
+
+  public DataForm getDF() {
+    return fromDF;
+  }
+}
