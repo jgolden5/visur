6c17e3b389d040ab10100642fcf38e35e67c9a78
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-05-14 13:39:17 -0700

    niDC.makeBrick works

--- src/test/com/ple/visur/TestDC.java
+++ src/test/com/ple/visur/TestDC.java
@@ -12,7 +12,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestDC {
   CursorPositionDCHolder cursorPositionDCHolder = CursorPositionDCHolder.make();
 
-  @Test void cdcMakeBrick() {
+  @Test void dcMakeBrick() {
+    //cdc makeBrick tests
     //cursorPositionDC.makeBrick works
     CompoundDataClassBrick cursorPositionDCB = cursorPositionDCHolder.cursorPositionDC.makeBrick(null, cursorPositionDCHolder);
     assertEquals(cursorPositionDCHolder.cursorPositionDC, cursorPositionDCB.getCDC());
@@ -28,6 +29,15 @@ public class TestDC {
     assertEquals(cursorPositionDCHolder.wholePairDC, cxcyDCB.getCDC());
     assertEquals(cursorPositionDCHolder.cxcycaDC, cxcyDCB.getOuter().getCDC());
 
+    //pdc makeBrick tests
+    //niDC.makeBrick works
+    ArrayList<Integer> newlineIndices = new ArrayList<>();
+    newlineIndices.add(11);
+    newlineIndices.add(24);
+    PrimitiveDataClassBrick niDCB = cursorPositionDCHolder.wholeNumberListDC.makeBrick(newlineIndices, cursorPositionDCB, cursorPositionDCHolder);
+    assertEquals(cursorPositionDCHolder.wholeNumberListDC, niDCB.getPDC());
+    assertEquals(newlineIndices, niDCB.getDFB().getVal());
+    assertEquals(cursorPositionDCHolder.cursorPositionDC, niDCB.getOuter().getCDC());
   }
 
 }
