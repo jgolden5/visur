df245f98db1a2815e881dc857c036521b693efd4
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-05-14 13:17:17 -0700

    cursorPositionDC.makeBrick and cxcycaDC.makeBrick work (tests passed)

--- src/test/com/ple/visur/TestDC.java
+++ src/test/com/ple/visur/TestDC.java
@@ -13,6 +13,16 @@ public class TestDC {
   CursorPositionDCHolder cursorPositionDCHolder = CursorPositionDCHolder.make();
 
   @Test void cdcMakeBrick() {
+    //cursorPositionDC.makeBrick works
+    CompoundDataClassBrick cursorPositionDCB = cursorPositionDCHolder.cursorPositionDC.makeBrick(null, cursorPositionDCHolder);
+    assertEquals(cursorPositionDCHolder.cursorPositionDC, cursorPositionDCB.getCDC());
+    assertNull(cursorPositionDCB.getOuter());
+
+    //cxcycaDC.makeBrick works
+    CompoundDataClassBrick cxcycaDCB = cursorPositionDCHolder.cxcycaDC.makeBrick(cursorPositionDCB, cursorPositionDCHolder);
+    assertEquals(cursorPositionDCHolder.cxcycaDC, cxcycaDCB.getCDC());
+    assertEquals(cursorPositionDCHolder.cursorPositionDC, cxcycaDCB.getOuter().getCDC());
+
 
   }
 
