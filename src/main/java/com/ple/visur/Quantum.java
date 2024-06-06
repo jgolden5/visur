ec585cb17a9ecf06ed72222fd15ad43d563f597c
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-06-06 10:12:18 -0700

    currentQuantum vs otherQuantum bound lengths get compared accurately and checked in the same spot

--- src/main/java/com/ple/visur/Quantum.java
+++ src/main/java/com/ple/visur/Quantum.java
@@ -10,12 +10,24 @@ public abstract class Quantum implements Shareable {
   abstract String getName();
   int quantumStart() {
     EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
-    Quantum currentQuantum = emc.getCurrentQuantum();
-    String currentQuantumName = currentQuantum.getName();
     ExecutionDataStack eds = emc.getExecutionDataStack();
     String otherQuantumName = (String)eds.pop();
-    String otherQuantumNameWithoutQuotes = otherQuantumName.substring(1, otherQuantumName.length() - 1); //did this just to make sure I don't get any unexpected behavior since currentQuantumName is not wrapped in extra quotes
-    System.out.println("quantumStart called. CurrentQ = " + currentQuantumName + ". OtherQ = " + otherQuantumNameWithoutQuotes);
+    String otherQuantumNameWithoutQuotes = otherQuantumName.substring(1, otherQuantumName.length() - 1);
+    QuantumNameToQuantum quantumNameToQuantum = emc.getQuantumNameToQuantum();
+    Quantum otherQuantum = quantumNameToQuantum.get(otherQuantumNameWithoutQuotes);
+    int distanceOfCurrentQuantumBounds = getQuantumBoundsLength();
+    int distanceOfOtherQuantumBounds = otherQuantum.getQuantumBoundsLength();
+    Quantum longerQuantum;
+    Quantum shorterQuantum;
+    if(distanceOfCurrentQuantumBounds > distanceOfOtherQuantumBounds) {
+      longerQuantum = this;
+      shorterQuantum = otherQuantum;
+    } else {
+      longerQuantum = otherQuantum;
+      shorterQuantum = this;
+    }
+    System.out.println("quantumStart called. CurrentQ = " + getName() + ". OtherQ = " + otherQuantumNameWithoutQuotes);
+    System.out.println("longer quantum = " + longerQuantum.getName() + "; shorter quantum = " + shorterQuantum.getName());
     return 0;
   }
 
@@ -29,4 +41,11 @@ public abstract class Quantum implements Shareable {
     System.out.println("quantumEnd called. CurrentQ = " + currentQuantumName + ". OtherQ = " + otherQuantumNameWithoutQuotes);
     return 0;
   }
+
+  int getQuantumBoundsLength() {
+    EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
+    int[] bounds = getBoundaries(emc.getEditorContent(), emc.getNewlineIndices(), false);
+    return bounds[1] - bounds[0];
+  }
+
 }
