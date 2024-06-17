f24342010763a227eae792e3f7b170bd223b154e
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-06-17 13:18:28 -0700

    getBoundaries method work for span 1 (word move in span 0 is now broken) rwb

--- src/main/java/com/ple/visur/RegexQuantum.java
+++ src/main/java/com/ple/visur/RegexQuantum.java
@@ -39,16 +39,33 @@ public class RegexQuantum extends Quantum {
     int leftBound = (int)caBVV.getVal();
     int rightBound = leftBound;
     int span = emc.getSpan();
+    leftBound = getQuantumStart(leftBound);
     while(span > 0) {
-      if(!isAtBeginningOfQuantum(rightBound)) {
-        rightBound = getNextQuantumStart(rightBound);
-      }
-      rightBound = getQuantumEnd(rightBound);
+      rightBound = getQuantumEnd(leftBound);
       span--;
     }
     bounds[0] = leftBound;
     bounds[1] = rightBound;
     return bounds;
+  }
+
+  private int getQuantumStart(int start) {
+    int leftBound = start;
+    boolean endFound = false;
+    while(!endFound) {
+      if(leftBound > 0) {
+        String strToMatch = emc.getStrToMatch(false, leftBound);
+        Matcher matcher = pattern.matcher(strToMatch);
+        if (matcher.matches()) {
+          leftBound--;
+        } else {
+          endFound = true;
+        }
+      } else {
+        endFound = true;
+      }
+    }
+    return leftBound;
   }
 
   private int getQuantumEnd(int start) {
@@ -57,7 +74,7 @@ public class RegexQuantum extends Quantum {
     boolean endFound = false;
     while(!endFound) {
       if(rightBound < editorContent.length()) {
-        CharSequence strToMatch = editorContent.substring(rightBound, rightBound + 1);
+        String strToMatch = emc.getStrToMatch(true, rightBound);
         Matcher matcher = pattern.matcher(strToMatch);
         if (matcher.matches()) {
           rightBound++;
@@ -67,89 +84,6 @@ public class RegexQuantum extends Quantum {
       } else {
         endFound = true;
       }
-    }
-    return rightBound;
-  }
-
-  /**
-   * make var matchIndex
-   * if firstCharMatches, search left for nonmatch
-   * else
-     * search right for match
-     * if NO MATCH is found, search left for match AND THEN left for nonmatch
-   * if no match is still found, return start
-   * else return matchIndex
-   * @param start
-   * @param editorContent
-   * @return
-   */
-  private int getLeftBound(boolean firstCharMatches, int start, String editorContent) {
-    int leftBound;
-    if(firstCharMatches) {
-      leftBound = goLeftUntilFound(false, start, editorContent);
-    } else {
-      leftBound = goRightUntilFound(true, start, editorContent);
-      if(leftBound == start) {
-        leftBound = goLeftUntilFound(true, start, editorContent);
-        leftBound = goLeftUntilFound(false, leftBound, editorContent);
-      }
-    }
-    return leftBound;
-  }
-
-  /**
-   * make var matchIndex
-   * search right for nonmatch
-   * if no match is found, return start
-   * else return matchIndex
-   * @param start
-   * @param editorContent
-   * @return
-   */
-  private int getRightBound(int start, String editorContent) {
-    return goRightUntilFound(false, start, editorContent);
-  }
-
-  private int goLeftUntilFound(boolean matchDesired, int start, String editorContent) {
-    int leftBound = start;
-    boolean searchConditionFound = false;
-    while(!searchConditionFound && leftBound > 0) {
-      String strToMatch = editorContent.substring(leftBound - 1, leftBound);
-      Matcher matcher = pattern.matcher(strToMatch);
-      boolean searchCondition = matchDesired ? matcher.matches() : !matcher.matches();
-      if (searchCondition) {
-        searchConditionFound = true;
-      } else {
-        leftBound--;
-      }
-    }
-    if(leftBound == 0 && !matchDesired) {
-      searchConditionFound = true;
-    }
-    if(!searchConditionFound) {
-      leftBound = start;
-    }
-    return leftBound;
-  }
-
-  private int goRightUntilFound(boolean matchDesired, int startingIndex, String editorContent) {
-    int rightBound = startingIndex;
-    boolean searchConditionFound = false;
-    while(!searchConditionFound && rightBound < editorContent.length() - 1) {
-      String strToMatch = editorContent.substring(rightBound, rightBound + 1);
-      Matcher matcher = pattern.matcher(strToMatch);
-      boolean searchCondition = matchDesired ? matcher.matches() : !matcher.matches();
-      if(searchCondition) {
-        searchConditionFound = true;
-      } else {
-        rightBound++;
-      }
-    }
-    if(rightBound == editorContent.length() - 1 && !matchDesired) {
-      searchConditionFound = true;
-    }
-    if(!searchConditionFound) {
-      rightBound = startingIndex;
     }
     return rightBound;
   }
@@ -205,6 +139,19 @@ public class RegexQuantum extends Quantum {
     return nextStringMatches && !prevStringMatches;
   }
 
+//  private boolean isAtEndOfQuantum(int bound) {
+//    String nextString = emc.getStrToMatch(true, bound);
+//    Matcher nextStringMatcher = pattern.matcher(nextString);
+//    boolean nextStringMatches = nextStringMatcher.matches();
+//    boolean prevStringMatches = false;
+//    if(bound < emc.getEditorContent().length()) {
+//      String prevString = emc.getStrToMatch(false, bound);
+//      Matcher prevStringMatcher = pattern.matcher(prevString);
+//      prevStringMatches = prevStringMatcher.matches();
+//    }
+//    return !nextStringMatches && prevStringMatches;
+//  }
+//
   private int getNextQuantumStart(int bound) {
     String editorContent = emc.getEditorContent();
     int nextStart = bound;
