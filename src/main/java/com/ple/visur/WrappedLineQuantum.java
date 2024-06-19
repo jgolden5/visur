b06cc4b78b604dd4a508ad8c46a08013440c5298
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-06-19 11:02:09 -0700

    wrappedLineQuantum.move with span 0 works, including moving from beginning of last line in editorContent to end rwb

--- src/main/java/com/ple/visur/WrappedLineQuantum.java
+++ src/main/java/com/ple/visur/WrappedLineQuantum.java
@@ -19,21 +19,31 @@ public class WrappedLineQuantum extends Quantum {
     int ca = (int)caBVV.getVal();
     BrickVisurVar cyBVV = (BrickVisurVar)emc.getGlobalVar("cy");
     int cy = (int)cyBVV.getVal();
+    int span = emc.getSpan();
     boolean lastCharIsNewline = editorContent.charAt(editorContent.length() - 1) == '\n';
     while(mv.dy != 0) {
       if(mv.dy > 0) {
         boolean canGoDown = lastCharIsNewline ? cy < newlineIndices.size() - 1 : cy < newlineIndices.size();
         if(canGoDown) {
           ca = cq.move(editorContent, newlineIndices, mv);
+        } else if(span == 0 && !canGoDown) {
+          ca = editorContent.length();
+          mv.dy--;
         } else {
           mv.dy--;
         }
       } else {
-        boolean canGoUp = cy > 0;
-        if(canGoUp) {
-          ca = cq.move(editorContent, newlineIndices, mv);
-        } else {
+        if(ca == editorContent.length() && editorContent.charAt(ca - 1) != '\n') {
+          int[] bounds = getBoundaries(editorContent, newlineIndices, span, false);
+          ca = bounds[0];
           mv.dy++;
+        } else {
+          boolean canGoUp = cy > 0;
+          if (canGoUp) {
+            ca = cq.move(editorContent, newlineIndices, mv);
+          } else {
+            mv.dy++;
+          }
         }
       }
     }
