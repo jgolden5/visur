ca2423e9a5fc52b00607c79d0aeff71ca688d5e5
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-06-19 10:17:19 -0700

    temp vars are stored and preserve the state of span and cursorQuantum before insert mode (still need to remove temp vars) rwb

--- src/main/java/com/ple/visur/ChangeQuantumOp.java
+++ src/main/java/com/ple/visur/ChangeQuantumOp.java
@@ -5,15 +5,36 @@ public class ChangeQuantumOp implements Operator {
   @Override
   public void execute(Object opInfo) {
     EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
-    String quantumName = (String)emc.getExecutionDataStack().pop();
-    Quantum targetQuantum = emc.getQuantumNameToQuantum().get(quantumName);
-    int[] bounds = targetQuantum.getBoundaries(emc.getEditorContent(), emc.getNewlineIndices(), emc.getSpan(), false);
-    emc.putCursorQuantumStart(bounds[0]);
-    emc.putCursorQuantumEnd(bounds[1]);
-    if(bounds[0] == bounds[1]) {
-      emc.putSpan(0);
+    String quantumName = getQuantumName(emc);
+    if(quantumName != null) {
+      Quantum targetQuantum = emc.getQuantumNameToQuantum().get(quantumName);
+      int[] bounds = targetQuantum.getBoundaries(emc.getEditorContent(), emc.getNewlineIndices(), emc.getSpan(), false);
+      emc.putCursorQuantumStart(bounds[0]);
+      emc.putCursorQuantumEnd(bounds[1]);
+      if (bounds[0] == bounds[1]) {
+        emc.putSpan(0);
+      }
+      emc.putCursorQuantum(targetQuantum);
     }
-    emc.putCursorQuantum(targetQuantum);
+  }
+
+  public String getQuantumName(EditorModelCoupler emc) {
+    String topElement = (String)emc.getExecutionDataStack().peek();
+    String qName;
+    boolean topElementIsInvalidQuantumName = emc.getQuantumNameToQuantum().get(topElement) == null;
+    if(topElementIsInvalidQuantumName) {
+      boolean quantumGlobalVarExistsWithTopElementName = emc.getGlobalVar(topElement).getVal() instanceof Quantum;
+      if(quantumGlobalVarExistsWithTopElementName) {
+        ObjectVisurVar qOVV = (ObjectVisurVar) emc.getGlobalVar(topElement);
+        Quantum q = (Quantum)qOVV.getVal();
+        qName = q.getName();
+      } else {
+        return null;
+      }
+    } else {
+      qName = (String) emc.getExecutionDataStack().pop();;
+    }
+    return qName;
   }
 
 }
