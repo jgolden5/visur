0e747a45d08e398e213664825cb64582bbeeebca
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-06-22 10:48:20 -0700

    make delete command work with CharacterQuantum (deletes currently selected quantum IF span > 0, and keeps ca the same)

--- src/main/java/com/ple/visur/DeleteCursorQuantumOp.java
+++ src/main/java/com/ple/visur/DeleteCursorQuantumOp.java
@@ -3,6 +3,20 @@ package com.ple.visur;
 public class DeleteCursorQuantumOp implements Operator {
   @Override
   public void execute(Object opInfo) {
-    System.out.println("delete cursor quantum");
+    EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
+    int span = emc.getSpan();
+    if(span > 0) {
+      int startBound = emc.getCursorQuantumStart();
+      int endBound = emc.getCursorQuantumEnd();
+      String editorContent = emc.getEditorContent();
+      String editorContentBeforeDeletedPortion = editorContent.substring(0, startBound);
+      String editorContentAfterDeletedPortion = editorContent.substring(endBound, editorContent.length());
+      String resultingEditorContent = editorContentBeforeDeletedPortion + editorContentAfterDeletedPortion;
+      emc.putEditorContent(resultingEditorContent);
+      Quantum cursorQuantum = emc.getCursorQuantum();
+      int[] bounds = cursorQuantum.getBoundaries(resultingEditorContent, emc.getNewlineIndices(), span, false);
+      emc.putCursorQuantumStart(bounds[0]);
+      emc.putCursorQuantumEnd(bounds[1]);
+    }
   }
 }
