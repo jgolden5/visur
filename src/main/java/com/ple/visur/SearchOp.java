a49001778571242ef862cedcada22b26637f119b
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-06-24 10:17:08 -0700

    make SearchOp search within whatever quantum current scope is for certain characters rwb

--- src/main/java/com/ple/visur/SearchOp.java
+++ src/main/java/com/ple/visur/SearchOp.java
@@ -6,13 +6,27 @@ public class SearchOp implements Operator {
     EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
     ExecutionDataStack eds = emc.getExecutionDataStack();
     String searchTarget = "";
-    if(eds.size() > 0) {
+    if (eds.size() > 0) {
       searchTarget = (String) eds.pop();
     }
-    if(searchTarget == "") {
+    if (searchTarget.equals("")) {
       System.out.println("No search target found");
     } else {
       System.out.println("search target = " + searchTarget);
+      String editorContent = emc.getEditorContent();
+      Quantum scopeQuantum = emc.getScopeQuantum();
+      int[] scopeQuantumBounds = scopeQuantum.getBoundaries(editorContent, emc.getNewlineIndices(), 1, false); //span is always 1 when searching within a scopeQuantum
+      int scopeQuantumEnd = scopeQuantumBounds[1];
+      int ca = emc.getCA();
+      String editorContentSubstringToSearch = editorContent.substring(ca, scopeQuantumEnd);
+      int foundLocation = editorContentSubstringToSearch.indexOf(searchTarget);
+      if(foundLocation > -1) {
+        emc.putCA(foundLocation);
+        Quantum cursorQuantum = emc.getCursorQuantum();
+        int[] newBounds = cursorQuantum.getBoundaries(editorContent, emc.getNewlineIndices(), emc.getSpan(), false);
+        emc.putCursorQuantumStart(newBounds[0]);
+        emc.putCursorQuantumEnd(newBounds[1]);
+      }
     }
   }
 }
