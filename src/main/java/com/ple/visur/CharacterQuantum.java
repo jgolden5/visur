70aa54422e73520d212b0254841609a364f1437d
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-08 10:22:49 -0700

    cq.moveUp designed with shortDC nr

--- src/main/java/com/ple/visur/CharacterQuantum.java
+++ src/main/java/com/ple/visur/CharacterQuantum.java
@@ -76,33 +76,17 @@ public class CharacterQuantum extends Quantum {
     int shortCY = emc.getShortCY();
     emc.putShortCY(++shortCY);
     int[] longBounds = emc.calcLongLineBoundaries(editorContent, newlineIndices, emc.getSpan(), false);
-    shortCX = Math.min(shortCX, longBounds[1]);
-    emc.putShortCX(shortCX);
+    emc.putShortCX(Math.min(shortCX, longBounds[1]));
     int ca = emc.getCA();
     return ca;
   }
 
   private int moveUp(String editorContent, ArrayList<Integer> newlineIndices) {
-    int cx = emc.getCX();
-    int cy = emc.getCY();
-    int canvasWidth = emc.getCanvasWidth();
-    int[] shortBounds = emc.calcShortLineBoundaries();
-    boolean isOnFirstShortLineInLongLine = shortBounds[0] == 0;
-    boolean isAtBeginningOfEditorContent = cy == 0 && cx == 0;
-    boolean shouldDecrementCY = isOnFirstShortLineInLongLine && !isAtBeginningOfEditorContent;
-    int virtualCX = emc.getVirtualCX();
-    int[] longBounds;
-    if(shouldDecrementCY) {
-      emc.putCY(--cy);
-      emc.putCX(0); //default for testing getLongLineBoundaries so old cx does not mismatch ca with new cy
-      longBounds = emc.calcLongLineBoundaries(editorContent, newlineIndices, 1, false);
-      int numberOfTrailingCharsAtEndOfLongLine = longBounds[1] - longBounds[1] % canvasWidth;
-      int numberOfShortLinesInLongLine = numberOfTrailingCharsAtEndOfLongLine > 0 ? (int)Math.ceil(longBounds[1] / (numberOfTrailingCharsAtEndOfLongLine)) : 1;
-      cx = virtualCX < longBounds[1] ? virtualCX * numberOfShortLinesInLongLine : longBounds[1];
-    } else if(!isAtBeginningOfEditorContent) {
-      cx = cx < canvasWidth ? 0 : cx - canvasWidth;
-    }
-    emc.putCX(cx);
+    int shortCX = emc.getShortCX();
+    int shortCY = emc.getShortCY();
+    emc.putShortCY(--shortCY);
+    int[] longBounds = emc.calcLongLineBoundaries(editorContent, newlineIndices, emc.getSpan(), false);
+    emc.putShortCX(Math.min(shortCX, longBounds[1]));
     int ca = emc.getCA();
     return ca;
   }
