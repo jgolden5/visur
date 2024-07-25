4dbd0a5d41c96eab76e5f48f93107e7649d39fd4
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-25 07:20:43 -0700

    add methods to ldc (all the methods cdc has) nr

--- src/main/java/DataClass/LayeredDataClass.java
+++ src/main/java/DataClass/LayeredDataClass.java
@@ -1,13 +1,14 @@
 package DataClass;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public abstract class LayeredDataClass implements DataClass {
 
   ArrayList<CompoundDataClass> layers = new ArrayList<>();
 
   @Override
-  public DataClassBrick makeBrick(String name, CompoundDataClassBrick outer) {
+  public LayeredDataClassBrick makeBrick(String name, CompoundDataClassBrick outer) {
     return null;
   }
 
@@ -19,9 +20,22 @@ public abstract class LayeredDataClass implements DataClass {
     return layers.get(i);
   }
 
-  public void calc(String name, LayeredDataClassBrick thisAsBrick) {
+  public void calcInLayers(String name, LayeredDataClassBrick thisAsBrick) {
     for(CompoundDataClass layer : layers) {
       layer.calcInternal(name, thisAsBrick);
+    }
+  }
+
+  public abstract ConflictsCheckResult conflictsCheck(LayeredDataClassBrick brick, String targetName, Object targetVal);
+  public void removeConflictingLayers(LayeredDataClassBrick thisAsBrick, String targetName, Object targetVal) {
+    ConflictsCheckResult ccr = conflictsCheck(thisAsBrick, targetName, targetVal);
+    if(ccr != ConflictsCheckResult.no) {
+      for(CompoundDataClassBrick layer : thisAsBrick.layers) {
+        if(layer.getName().equals(targetName) || layer.containsName(targetName)) {
+          continue;
+        }
+        layer.remove();
+      }
     }
   }
 
