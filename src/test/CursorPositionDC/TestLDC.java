4b2c0398c79e3f95405da9c43486417bfa0eebde
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-25 17:00:17 -0700

    caAndNLDC.putAndGetLayer(rcxcyAndNLDC) test passed

--- src/test/CursorPositionDC/TestLDC.java
+++ src/test/CursorPositionDC/TestLDC.java
@@ -3,17 +3,23 @@ package CursorPositionDC;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestLDC {
 
   CursorPositionDC cursorPositionDC = new CursorPositionDC();
   CAAndNLDC caAndNLDC = new CAAndNLDC(2);
+  RCXCYAndNLDC rcxcyAndNLDC = new RCXCYAndNLDC(3);
 
   @Test
   void putAndGetLayer() {
     cursorPositionDC.putLayer(caAndNLDC);
     assertEquals(caAndNLDC, cursorPositionDC.getLayer(0));
+
+    cursorPositionDC.putLayer(rcxcyAndNLDC);
+    assertEquals(rcxcyAndNLDC, cursorPositionDC.getLayer(1));
   }
 
   @BeforeEach
