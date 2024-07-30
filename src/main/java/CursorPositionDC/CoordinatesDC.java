aa7375f929b8949cc6a94bd053bbac5c035eb95f
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-30 14:30:34 -0700

    make getAllCoordinatePDCBVals method to extract all vars from coordinatesDCB nr

--- src/main/java/CursorPositionDC/CoordinatesDC.java
+++ src/main/java/CursorPositionDC/CoordinatesDC.java
@@ -36,40 +36,50 @@ public class CoordinatesDC extends CompoundDataClass {
   @Override
   public Result<PrimitiveDataClassBrick> calcInternal(String name, OuterDataClassBrick thisAsBrick) {
     Result<PrimitiveDataClassBrick> r;
-    if(name.equals("ca")) {
-      r = calcCAAndNL(thisAsBrick);
-    } else if(name.equals("cy")) {
-      r = calcRCXCYAndNL(thisAsBrick);
-    } else {
-      r = Result.make(null, "name not recognized");
-    }
-    return r;
-  }
-
-  private Result<PrimitiveDataClassBrick> calcCAAndNL(OuterDataClassBrick thisAsBrick) {
-    Result<PrimitiveDataClassBrick> r;
     if(thisAsBrick.isComplete()) {
+      Object[] coordinatesVars = getAllCoordinatePDCBVals(thisAsBrick);
       CompoundDataClassBrick thisAsCDCB = (CompoundDataClassBrick) thisAsBrick;
-      CompoundDataClassBrick rcxCYAndNLDCB = (CompoundDataClassBrick) thisAsCDCB.getInner("rcxCYAndNL");
-      PrimitiveDataClassBrick rcxDCB = (PrimitiveDataClassBrick) rcxCYAndNLDCB.getInner("rcx");
-      PrimitiveDataClassBrick cyDCB = (PrimitiveDataClassBrick) rcxCYAndNLDCB.getInner("cy");
-      PrimitiveDataClassBrick nlDCB = (PrimitiveDataClassBrick) rcxCYAndNLDCB.getInner("nl");
-      int rcx = (int)rcxDCB.getVal();
-      int cy = (int)cyDCB.getVal();
-      ArrayList<Integer> nl = (ArrayList<Integer>) nlDCB.getVal();
-      int lineStart = cy > 0 ? nl.get(cy) : 0;
-      int ca = lineStart + rcx;
-      CompoundDataClassBrick caAndNLDCB = (CompoundDataClassBrick) thisAsCDCB.getInner("caAndNL");
-      PrimitiveDataClassBrick caDCB = (PrimitiveDataClassBrick) caAndNLDCB.getInner("ca");
-      caDCB.put(ca);
-      r = Result.make(ca, null);
+      if (name.equals("ca")) {
+        r = calcCAAndNL(thisAsCDCB, coordinatesVars);
+      } else if (name.equals("cy")) {
+        r = calcRCXCYAndNL(thisAsCDCB, coordinatesVars);
+      } else {
+        r = Result.make(null, "name not recognized");
+      }
     } else {
       r = Result.make(null, "brick incomplete, calculations impossible");
     }
     return r;
   }
 
-  private Result<PrimitiveDataClassBrick> calcRCXCYAndNL(OuterDataClassBrick thisAsBrick) {
+  private Object[] getAllCoordinatePDCBVals(OuterDataClassBrick thisAsBrick) {
+    CompoundDataClassBrick thisAsCDCB = (CompoundDataClassBrick) thisAsBrick;
+    CompoundDataClassBrick caAndNLDCB = (CompoundDataClassBrick) thisAsCDCB.getInner("caAndNL");
+    PrimitiveDataClassBrick caDCB = (PrimitiveDataClassBrick) caAndNLDCB.getInner("ca");
+    PrimitiveDataClassBrick nlDCB = (PrimitiveDataClassBrick) caAndNLDCB.getInner("nl");
+    CompoundDataClassBrick rcxCYAndNLDCB = (CompoundDataClassBrick) thisAsCDCB.getInner("rcxCYAndNL");
+    PrimitiveDataClassBrick rcxDCB = (PrimitiveDataClassBrick) rcxCYAndNLDCB.getInner("rcx");
+    PrimitiveDataClassBrick cyDCB = (PrimitiveDataClassBrick) rcxCYAndNLDCB.getInner("cy");
+    int ca = caDCB.isComplete() ? (int)caDCB.getVal() : -1;
+    ArrayList<Integer> nl = nlDCB.isComplete() ? (ArrayList<Integer>) nlDCB.getVal() : null;
+    int rcx = rcxDCB.isComplete() ? (int)rcxDCB.getVal() : -1;
+    int cy = cyDCB.isComplete() ? (int)cyDCB.getVal() : -1;
+    return new Object[]{ca, nl, rcx, cy};
+  }
+
+  private Result<PrimitiveDataClassBrick> calcCAAndNL(CompoundDataClassBrick thisAsCDCB, Object[] coordinateVars) {
+    ArrayList<Integer> nl = (ArrayList<Integer>)coordinateVars[1];
+    int rcx = (int)coordinateVars[2];
+    int cy = (int)coordinateVars[3];
+    int lineStart = cy > 0 ? nl.get(cy) : 0;
+    int ca = lineStart + rcx;
+    CompoundDataClassBrick caAndNLDCB = (CompoundDataClassBrick) thisAsCDCB.getInner("caAndNL");
+    PrimitiveDataClassBrick caDCB = (PrimitiveDataClassBrick) caAndNLDCB.getInner("ca");
+    caDCB.put(ca);
+    return Result.make(ca, null);
+  }
+
+  private Result<PrimitiveDataClassBrick> calcRCXCYAndNL(CompoundDataClassBrick thisAsCDCB, Object[] coordinateVars) {
     return null;
   }
 
