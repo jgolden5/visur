e1d15e3c0ae140a5b397a07cdc72eae3d9ad364d
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-08-02 12:54:47 -0700

    llFromCYDC.makeBrick make bricks for cyAndNL and llcyAndNL nr

--- src/main/java/CursorPositionDC/LLFromCYDC.java
+++ src/main/java/CursorPositionDC/LLFromCYDC.java
@@ -3,6 +3,7 @@ package CursorPositionDC;
 import DataClass.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LLFromCYDC extends CompoundDataClass {
   public LLFromCYDC(int requiredSetValues) {
@@ -11,7 +12,25 @@ public class LLFromCYDC extends CompoundDataClass {
 
   @Override
   public CompoundDataClassBrick makeBrick(String name, ArrayList<OuterDataClassBrick> outers, PrimitiveDataClassBrick... reusablePDCBs) {
-    return null;
+    CompoundDataClassBrick llFromCYDCB = CompoundDataClassBrick.make(name, outers, this, new HashMap<>());
+    HashMap<String, DataClassBrick> llFromCYInners = new HashMap<>();
+
+    PrimitiveDataClassBrick nlDCB = reusablePDCBs[0];
+    PrimitiveDataClassBrick cyDCB = reusablePDCBs[1];
+    PrimitiveDataClassBrick llDCB = reusablePDCBs[2];
+
+    CYAndNLDC cyAndNLDC = (CYAndNLDC) getInner("cyAndNL");
+    CompoundDataClassBrick cyAndNLDCB = cyAndNLDC.makeBrick("cyAndNL", new ArrayList<>(), nlDCB, cyDCB);
+    cyAndNLDCB.putOuter(llFromCYDCB);
+
+    LLCYAndNLDC llcyAndNLDC = (LLCYAndNLDC) getInner("llcyAndNL");
+    CompoundDataClassBrick llcyAndNLDCB = llcyAndNLDC.makeBrick("llcyAndNL", new ArrayList<>(), nlDCB, cyDCB, llDCB);
+    llcyAndNLDCB.putOuter(llFromCYDCB);
+
+    llFromCYInners.put("cyAndNL", cyAndNLDCB);
+    llFromCYInners.put("llcyAndNL", llcyAndNLDCB);
+
+    return llFromCYDCB.getInitializedBrickFromInners(llFromCYInners);
   }
 
   @Override
