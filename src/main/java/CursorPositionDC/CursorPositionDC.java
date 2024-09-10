79b183dfd9ad48f635c3ffa904107954154226ef
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-10 16:31:00 -0700

    update CursorPositionDC.makeBrick to only call coordinatesDC.makeBrick nr

--- src/main/java/CursorPositionDC/CursorPositionDC.java
+++ src/main/java/CursorPositionDC/CursorPositionDC.java
@@ -16,24 +16,12 @@ public class CursorPositionDC extends LayeredDataClass {
     LayeredDataClassBrick cursorPositionDCB = LayeredDataClassBrick.make(name, this, new ArrayList<>());
 
     PrimitiveDataClassBrick nlDCB = reusablePDCBs[0];
-    PrimitiveDataClassBrick rcxDCB = reusablePDCBs[1];
-    PrimitiveDataClassBrick cyDCB = reusablePDCBs[2];
-    PrimitiveDataClassBrick llDCB = reusablePDCBs[3];
 
     CoordinatesDC coordinatesDC = (CoordinatesDC) getLayer(0);
-    CompoundDataClassBrick coordinatesDCB = coordinatesDC.makeBrick("coordinates", new ArrayList<>(), nlDCB, rcxDCB, cyDCB);
+    CompoundDataClassBrick coordinatesDCB = coordinatesDC.makeBrick("coordinates", new ArrayList<>(), nlDCB);
     coordinatesDCB.putOuter(cursorPositionDCB);
 
-    LLFromCYDC llFromCYDC = (LLFromCYDC) getLayer(1);
-    CompoundDataClassBrick llFromCYDCB = llFromCYDC.makeBrick("llFromCY", new ArrayList<>(), nlDCB, cyDCB, llDCB);
-
-    VirtualDC virtualDC = (VirtualDC) getLayer(2);
-    CompoundDataClassBrick virtualDCB = virtualDC.makeBrick("virtual", new ArrayList<>(), rcxDCB, llDCB);
-    virtualDCB.putOuter(cursorPositionDCB);
-
     cursorPositionDCB.putLayer(coordinatesDCB);
-    cursorPositionDCB.putLayer(llFromCYDCB);
-    cursorPositionDCB.putLayer(virtualDCB);
 
     return cursorPositionDCB;
   }
