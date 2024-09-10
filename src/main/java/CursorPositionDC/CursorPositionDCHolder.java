65575306d2eba6647c50d2ec8215aa0c5c2055e6
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-10 16:24:36 -0700

    get rid of all dc's & dcb's in TestCursorPositionDC that are involved only in virtualDC-related vars rwb

--- src/main/java/CursorPositionDC/CursorPositionDCHolder.java
+++ src/main/java/CursorPositionDC/CursorPositionDCHolder.java
@@ -9,13 +9,8 @@ public class CursorPositionDCHolder extends DCHolder implements Shareable {
   public final IntArrayListDF intArrayListDF = new IntArrayListDF(0);
   public final CursorPositionDC cursorPositionDC = new CursorPositionDC();
   public final CoordinatesDC coordinatesDC = new CoordinatesDC(1);
-  public final LLFromCYDC llFromCYDC = new LLFromCYDC(1);
-  public final VirtualDC virtualDC = new VirtualDC(2);
   public final CAAndNLDC caAndNLDC = new CAAndNLDC(2);
   public final RCXCYAndNLDC rcxcyAndNLDC = new RCXCYAndNLDC(3);
-  public final CYAndNLDC cyAndNLDC = new CYAndNLDC(2);
-  public final LLCYAndNLDC llcyAndNLDC = new LLCYAndNLDC(3);
-  public final VCXRCXAndLODC vcxRcxAndLODC = new VCXRCXAndLODC(2);
   public final WholeNumberDC wholeNumberDC = new WholeNumberDC(javaIntDF);
   public final WholeNumberListDC wholeNumberListDC = new WholeNumberListDC(intArrayListDF);
 
@@ -28,20 +23,9 @@ public class CursorPositionDCHolder extends DCHolder implements Shareable {
     caAndNLDC.putInner("nl", wholeNumberListDC);
     rcxcyAndNLDC.putInner("wholeNumber", wholeNumberDC);
     rcxcyAndNLDC.putInner("nl", wholeNumberListDC);
-    cyAndNLDC.putInner("wholeNumber", wholeNumberDC);
-    cyAndNLDC.putInner("nl", wholeNumberListDC);
-    llcyAndNLDC.putInner("wholeNumber", wholeNumberDC);
-    llcyAndNLDC.putInner("nl", wholeNumberListDC);
-    vcxRcxAndLODC.putInner("wholeNumber", wholeNumberDC);
     coordinatesDC.putInner("caAndNL", caAndNLDC);
     coordinatesDC.putInner("rcxcyAndNL", rcxcyAndNLDC);
-    llFromCYDC.putInner("cyAndNL", cyAndNLDC);
-    llFromCYDC.putInner("llcyAndNL", llcyAndNLDC);
-    virtualDC.putInner("wholeNumber", wholeNumberDC);
-    virtualDC.putInner("vcxRCXAndLO", vcxRcxAndLODC);
     cursorPositionDC.putLayer(coordinatesDC);
-    cursorPositionDC.putLayer(llFromCYDC);
-    cursorPositionDC.putLayer(virtualDC);
   }
 
 }
