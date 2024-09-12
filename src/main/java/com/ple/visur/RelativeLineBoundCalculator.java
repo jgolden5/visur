c14d43d026cb4be462621865cce1f591ff7d8544
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-12 10:32:19 -0700

    add RelativeLineBoundCalculator class, and re-implement RelativeLineBoundCalculator.getShort, getShortLineStart, and getShortLineEnd nr

---
+++ src/main/java/com/ple/visur/RelativeLineBoundCalculator.java
@@ -0,0 +1,43 @@
+package com.ple.visur;
+
+import java.util.ArrayList;
+
+public class RelativeLineBoundCalculator {
+
+  public static int[] getShort(int cx, int cy, ArrayList<Integer> nl) {
+    EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
+    int[] shortBounds = new int[2];
+    int canvasWidth = emc.getCanvasWidth();
+    shortBounds[0] = getShortLineStart(canvasWidth, cx);
+    shortBounds[1] = getShortLineEnd(shortBounds[0], canvasWidth, cx, cy, nl);
+    return shortBounds;
+  }
+
+  private static int getShortLineStart(int cw, int cx) {
+    int shortLineStart = 0;
+    for (int i = cx; i > 0; i--) {
+      if (i % cw == 0) {
+        shortLineStart = i;
+        break;
+      }
+    }
+    return shortLineStart;
+  }
+
+  private static int getShortLineEnd(int shortLineStart, int cw, int cx, int cy, ArrayList<Integer> nl) {
+    final int longLineLength = getLongLineLength(cy, nl);
+    int shortLineEnd = longLineLength;
+    if (shortLineEnd != shortLineStart) {
+      for (int i = cx + 1; i < longLineLength; i++) {
+        if (i % cw == 0) {
+          shortLineEnd = i;
+          break;
+        }
+      }
+    }
+    return shortLineEnd;
+  }
+
+}
+
+
