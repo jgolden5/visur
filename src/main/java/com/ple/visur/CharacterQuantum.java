c74a212074c772dcaeb06282e0347129157ffd1c
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-12 11:43:51 -0700

    fix cq.moveDown when on penultimate short line with span 1 rwb

--- src/main/java/com/ple/visur/CharacterQuantum.java
+++ src/main/java/com/ple/visur/CharacterQuantum.java
@@ -79,7 +79,7 @@ public class CharacterQuantum extends Quantum {
       vcx += canvasWidth;
     }
     int longLineLength = RelativeLineBoundCalculator.getLongLineLength(cy, nextLineIndices);
-    int longLineLimit = longLineLength;
+    int longLineLimit = span > 0 ? longLineLength - 1 : longLineLength;
     cx = Math.min(vcx, longLineLimit);
     emc.putCX(cx);
     emc.putVirtualCX(vcx);
--- src/main/java/com/ple/visur/RelativeLineBoundCalculator.java
+++ src/main/java/com/ple/visur/RelativeLineBoundCalculator.java
@@ -9,7 +9,7 @@ public class RelativeLineBoundCalculator {
     if(cy < nl.size()) {
       longLineLength = nl.get(cy) - 1;
       if(cy > 0) {
-        longLineLength -= nl.get(cy) - 1 - nl.get(cy - 1);
+        longLineLength -= nl.get(cy - 1) - 1;
       }
     } else {
       System.out.println("cy value is invalid");;
