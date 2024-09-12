90b642fc74f31a6201e9217ee18f1e4b99c11b52
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-12 10:45:24 -0700

    get rid of moveDown/Up/Left/RightIfPossible logic, as the methods take care of such logic by themselves with minimal additional logic rwb

--- src/main/java/com/ple/visur/CharacterQuantum.java
+++ src/main/java/com/ple/visur/CharacterQuantum.java
@@ -16,90 +16,23 @@ public class CharacterQuantum extends Quantum {
 
   @Override
   public int move(String editorContent, ArrayList<Integer> nextLineIndices, MovementVector mv) {
-    int canvasWidth = emc.getCanvasWidth();
     int realCA = emc.getCA();
     int span = emc.getSpan();
     if(mv.dx != 0) {
-      realCA = moveRightOrLeftIfPossible(realCA, mv, nextLineIndices, span);
+      if(mv.dx > 0) {
+        realCA = moveRight(span);
+      } else {
+        realCA = moveLeft();
+      }
     }
     if(mv.dy != 0) {
-      realCA = moveDownOrUpIfPossible(realCA, mv, nextLineIndices, span, canvasWidth);
+      if(mv.dy > 0) {
+        realCA = moveDown(editorContent, nextLineIndices, span);
+      } else {
+        realCA = moveUp(editorContent, nextLineIndices, span);
+      }
     }
     return realCA;
-  }
-
-  private int moveRightOrLeftIfPossible(int realCA, MovementVector mv, ArrayList<Integer> nextLineIndices, int span) {
-    int destinationCA = realCA;
-    while(mv.dx != 0) {
-      if(mv.dx > 0) {
-        boolean canMoveRight = checkCanMoveRight(realCA, nextLineIndices, span);
-        if(canMoveRight) {
-          destinationCA = moveRight(span);
-        }
-        mv.dx--;
-      } else {
-        boolean canMoveLeft = checkCanMoveLeft(realCA);
-        if(canMoveLeft) {
-          destinationCA = moveLeft();
-        }
-        mv.dx++;
-      }
-    }
-    return destinationCA;
-  }
-
-  private int moveDownOrUpIfPossible(int realCA, MovementVector mv, ArrayList<Integer> nextLineIndices, int span, int canvasWidth) {
-    int destinationCA = realCA;
-    String editorContent = emc.getEditorContent();
-    while(mv.dy != 0) {
-      if(mv.dy > 0) {
-        boolean canMoveDown = checkCanMoveDown(nextLineIndices, span, canvasWidth);
-        if(canMoveDown) {
-          destinationCA = moveDown(editorContent, nextLineIndices, span);
-        }
-        mv.dy--;
-      } else {
-        boolean canMoveUp = checkCanMoveUp();
-        if(canMoveUp) {
-          destinationCA = moveUp(editorContent, nextLineIndices, span);
-        }
-        mv.dy++;
-      }
-    }
-    return destinationCA;
-  }
-
-  private boolean checkCanMoveRight(int realCA, ArrayList<Integer> nextLineIndices, int span) {
-    int editorContentEnd = nextLineIndices.get(nextLineIndices.size() - 1);
-    boolean canMoveRight;
-    if(span > 0) {
-      canMoveRight = realCA + 1 < editorContentEnd;
-    } else {
-      canMoveRight = realCA + 1 <= editorContentEnd;
-    }
-    return canMoveRight;
-  }
-
-  private boolean checkCanMoveLeft(int realCA) {
-    return realCA > 0;
-  }
-
-  private boolean checkCanMoveDown(ArrayList<Integer> nextLineIndices, int span, int canvasWidth) {
-    boolean canMoveDown = true;
-    int rcx = emc.getCX();
-    int cy = emc.getCY();
-    if(cy >= nextLineIndices.size() - 1) {
-      int lastNewlineIndex = nextLineIndices.get(nextLineIndices.size() - 1);
-      int lastValidCX = span > 0 ? lastNewlineIndex - 1 : lastNewlineIndex;
-      if(rcx + canvasWidth > lastValidCX) {
-        canMoveDown = false;
-      }
-    }
-    return canMoveDown;
-  }
-
-  private boolean checkCanMoveUp() {
-    return false;
   }
 
   private int moveRight(int span) {
