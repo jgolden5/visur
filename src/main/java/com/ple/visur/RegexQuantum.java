047d10b0b2677be8f2b354140c65f12b5acbe4af
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-13 14:36:50 -0700

    rq.moveRight horizontal movement accurately triggers increment/decrementCanvasStart when ca > canvasEnd or ca < canvasStart respectively

--- src/main/java/com/ple/visur/RegexQuantum.java
+++ src/main/java/com/ple/visur/RegexQuantum.java
@@ -152,6 +152,11 @@ public class RegexQuantum extends Quantum {
       destination = cq.move(editorContent, newlineIndices, mv);
       mv.dy -= mv.dy > 0 ? 1 : -1;
     }
+    if(destination > emc.getCanvasEnd()) {
+      emc.incrementCanvasStart();
+    } else if(destination < emc.getCanvasStart()) {
+      emc.decrementCanvasStart();
+    }
     return destination;
   }
 
