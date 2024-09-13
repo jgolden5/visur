388e361c5694e3b9df4722680f948633cca32910
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-12 17:09:47 -0700

    fixed insert mode and backspace rwb

--- src/main/java/com/ple/visur/DeletePreviousCharOp.java
+++ src/main/java/com/ple/visur/DeletePreviousCharOp.java
@@ -4,13 +4,12 @@ public class DeletePreviousCharOp implements Operator {
   @Override
   public void execute(Object opInfo) {
     EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
-    BrickVisurVar realCABVV = (BrickVisurVar) emc.getGlobalVar("realCA");
-    int realCA = (int)realCABVV.getVal();
+    int ca = emc.getCA();
     String editorContent = emc.getEditorContent();
-    String editorContentBeforeChar = realCA > 0 ? editorContent.substring(0, realCA - 1) : "";
-    String editorContentAfterChar = editorContent.substring(realCA, editorContent.length());
+    String editorContentBeforeChar = ca > 0 ? editorContent.substring(0, ca - 1) : "";
+    String editorContentAfterChar = editorContent.substring(ca, editorContent.length());
     String resultingEditorContent = editorContentBeforeChar + editorContentAfterChar;
     emc.putEditorContent(resultingEditorContent);
-    emc.putGlobalVar("realCA", realCABVV);
+    emc.putCA(ca);
   }
 }
--- src/main/java/com/ple/visur/InsertNewlineOp.java
+++ src/main/java/com/ple/visur/InsertNewlineOp.java
@@ -5,15 +5,15 @@ public class InsertNewlineOp implements Operator {
   public void execute(Object opInfo) {
     EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
     String editorContent = emc.getEditorContent();
-    int realCA = emc.getCA();
-    String contentBeforeChar = editorContent.substring(0, realCA);
-    String contentAfterChar = editorContent.substring(realCA, editorContent.length());
+    int ca = emc.getCA();
+    String contentBeforeChar = editorContent.substring(0, ca);
+    String contentAfterChar = editorContent.substring(ca, editorContent.length());
     String resultingEditorContent = contentBeforeChar + "\n" + contentAfterChar;
     emc.putEditorContent(resultingEditorContent);
-    emc.putCA(++realCA);
+    emc.putCA(++ca);
     emc.updateNextLineIndices();
     Quantum cursorQuantum = emc.getCursorQuantum();
-    int[] bounds = cursorQuantum.getBoundaries(realCA, emc.getNextLineIndices(), emc.getSpan(), false);
+    int[] bounds = cursorQuantum.getBoundaries(ca, emc.getNextLineIndices(), emc.getSpan(), false);
     emc.putCursorQuantumStart(bounds[0]);
     emc.putCursorQuantumEnd(bounds[1]);
   }
