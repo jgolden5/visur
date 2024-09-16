9d4f6c5c4ddba9d89885d310fe09c9d0b0e68696
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-16 08:09:56 -0700

    scrolls automatically if switching to quantum moves start or end bound of quantum to higher or lower than canvasStart/End respectively

--- src/main/java/com/ple/visur/ChangeCursorQuantumOp.java
+++ src/main/java/com/ple/visur/ChangeCursorQuantumOp.java
@@ -14,6 +14,15 @@ public class ChangeCursorQuantumOp implements Operator {
       if (bounds[0] == bounds[1]) {
         emc.putSpan(0);
       }
+      if(emc.getCanvasStart() > bounds[0]) {
+        while (emc.getCanvasStart() > bounds[0]) {
+          emc.decrementCanvasStart();
+        }
+      } else {
+        while (emc.getCanvasEnd() < bounds[1]) {
+          emc.incrementCanvasStart();
+        }
+      }
       emc.putCursorQuantum(targetQuantum);
     }
   }
