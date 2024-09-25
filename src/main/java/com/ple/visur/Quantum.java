8e510337211a4a0c9ca901dcf8472103d50f6bf0
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-09-25 15:00:39 -0700

    simplified quantumStart/End to both contain < 20 lines and still work

--- src/main/java/com/ple/visur/Quantum.java
+++ src/main/java/com/ple/visur/Quantum.java
@@ -9,8 +9,10 @@ public abstract class Quantum implements Shareable {
   abstract int move(String editorContent, ArrayList<Integer> newlineIndices, MovementVector m);
   abstract String getName();
   EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
+
   void quantumStart() {
-    int spanMinimumOne = Math.max(emc.getSpan(), 1);
+    int actualSpan = emc.getSpan();
+    int spanMinimumOne = Math.max(actualSpan, 1);
     Quantum[] scopeAndCursorQuantums = getScopeAndCursorQuantums(spanMinimumOne);
     Quantum scopeQuantum = scopeAndCursorQuantums[0];
     Quantum cursorQuantum = scopeAndCursorQuantums[1];
@@ -20,14 +22,17 @@ public abstract class Quantum implements Shareable {
     emc.putCA(ca);
     emc.putVirtualCX(emc.getCX());
     int[] newCursorBounds = cursorQuantum.getBoundaries(ca, emc.getNextLineIndices(), spanMinimumOne, false);
-    int actualSpan = emc.getSpan();
     if(actualSpan == 0) {
       newCursorBounds[1] = newCursorBounds[0];
     }
     emc.putCursorQuantumStartAndScroll(newCursorBounds[0]);
     emc.putCursorQuantumEndAndScroll(newCursorBounds[1]);
-    if(cursorQuantum.equals(emc.getCursorQuantum())) {
-      emc.putCursorQuantum(cursorQuantum);
+    setSmallestQuantum(cursorQuantum);
+  }
+
+  private void setSmallestQuantum(Quantum smallestQuantum) {
+    if(!smallestQuantum.equals(emc.getCursorQuantum())) {
+      emc.putCursorQuantum(smallestQuantum);
     }
   }
 
@@ -51,24 +56,11 @@ public abstract class Quantum implements Shareable {
   }
 
   void quantumEnd() {
-    EditorModelCoupler emc = ServiceHolder.editorModelCoupler;
-    ExecutionDataStack eds = emc.getExecutionDataStack();
-    String otherQuantumName = (String)eds.pop();
-    QuantumNameToQuantum quantumNameToQuantum = emc.getQuantumNameToQuantum();
-    Quantum quantumFromStack = quantumNameToQuantum.get(otherQuantumName);
-    int spanMinimumOne = Math.max(emc.getSpan(), 1);
     int actualSpan = emc.getSpan();
-    int distanceOfCurrentQuantumBounds = getQuantumBoundsLengthAtSpan(spanMinimumOne);
-    int distanceOfOtherQuantumBounds = quantumFromStack.getQuantumBoundsLengthAtSpan(spanMinimumOne);
-    Quantum scopeQuantum;
-    Quantum cursorQuantum;
-    if(distanceOfCurrentQuantumBounds > distanceOfOtherQuantumBounds) {
-      scopeQuantum = this;
-      cursorQuantum = quantumFromStack;
-    } else {
-      scopeQuantum = quantumFromStack;
-      cursorQuantum = actualSpan == 0 ? quantumNameToQuantum.get("character") : this;
-    }
+    int spanMinimumOne = Math.max(actualSpan, 1);
+    Quantum[] scopeAndCursorQuantums = getScopeAndCursorQuantums(spanMinimumOne);
+    Quantum scopeQuantum = scopeAndCursorQuantums[0];
+    Quantum cursorQuantum = scopeAndCursorQuantums[1];
     int ca = emc.getCA();
     int[] scopeBounds = scopeQuantum.getBoundaries(ca, emc.getNextLineIndices(), spanMinimumOne, false);
     ca = actualSpan > 0 ? scopeBounds[1] - 1 : scopeBounds[1];
@@ -77,10 +69,7 @@ public abstract class Quantum implements Shareable {
     int[] newCursorBounds = cursorQuantum.getBoundaries(ca, emc.getNextLineIndices(), spanMinimumOne, false);
     emc.putCursorQuantumStartAndScroll(newCursorBounds[0]);
     emc.putCursorQuantumEndAndScroll(newCursorBounds[1]);
-    if(quantumFromStack.getName().equals(cursorQuantum.getName())) {
-      emc.putCursorQuantum(quantumFromStack);
-      System.out.println("quantum changed from " + getName() + " to " + quantumFromStack.getName());
-    }
+    setSmallestQuantum(cursorQuantum);
   }
 
   int getQuantumBoundsLengthAtSpan(int span) {
